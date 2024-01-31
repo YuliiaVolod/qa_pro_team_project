1a2724357296a54641e7d3d8220905ea03f109a7
Yuliia <stepaika74@gmail.com>
2024-02-01 00:08:38 +0200

    Скоротила код

--- src/js/fineList.js
+++ src/js/fineList.js
@@ -1,23 +1,14 @@
 "use strict";
 window.fineList = {
-    searchFines : searchFines
+    searchFines: searchFines
 }
 
 
 //Ця зміна містить всі дані які в нас зберігаються у файлі data
 let DB = data.finesData;
 
-function searchFines(searchKey){
-
+function searchFines(searchKey) {
     const re = /\d{3}/;
-    if (re.test(searchKey)){
-        // відповідає шаблону ХХХ - шукаєм по номеру
-        return DB.filter((item) => item.номер == searchKey);
-    }else{
-        // шукаєм по типу
-        return DB.filter((item) => item.тип == searchKey);
-    }
+    return DB.filter((item) => re.test(searchKey) ? item.номер === searchKey : item.тип === searchKey);
 }
-
-
 
