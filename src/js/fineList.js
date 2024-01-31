071fd18e0d9d8b1af1c239c078b1faada558e7af
Yuliia <stepaika74@gmail.com>
2024-02-01 00:47:42 +0200

    удосконалювала

--- src/js/fineList.js
+++ src/js/fineList.js
@@ -3,12 +3,10 @@ window.fineList = {
     searchFines: searchFines
 }
 
-
-//Ця зміна містить всі дані які в нас зберігаються у файлі data
 let DB = data.finesData;
 
 function searchFines(searchKey) {
     const re = /\d{3}/;
-    return DB.filter((item) => re.test(searchKey) ? item.номер === searchKey : item.тип === searchKey);
+    return DB.filter((item) => re.test(searchKey)? item.номер === searchKey: item.тип === searchKey);
 }
 
